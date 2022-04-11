6af7a3042c70aba5b6b1e92dfeaadd144719a396
Minsunging <gamesung06@naver.com>
2022-04-11 19:47:28 +0900

    hi
    you're idiot

---
+++ minsungand/app/src/main/java/com/insang/sibal/siiibal.kt
@@ -0,0 +1,16 @@
+package com.insang.sibal
+
+class siiibal {
+}
+
+fun main() {
+    var h1 = "hello world"
+    var ss = 12
+    println(h1)
+    print(h1)
+    print(ss)
+}
+
+fun hello(): String{
+    return ""
+}
---
+++ minsungand/app/src/main/res/hello world.kt
@@ -0,0 +1,7 @@
+package com.insang.sibal
+
+fun main() {
+    val h1 = " hello world"
+    var h2: String = "hello world"
+
+}
